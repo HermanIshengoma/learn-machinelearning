ca5c08166f9b49571d1347666c3348ad361e77a3
HermanIshengoma <62037573+HermanIshengoma@users.noreply.github.com>
2021-03-02 13:53:21 -0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, request, make_response, redirect, url_for
+from flask import render_template, jsonify, session
+
+app = Flask(__main__)
+
+
+@app.route('/')
+def start_page():
+	html = render_template('index.html')
+	return make_response(html)
+
+
+if __name__ == "__main__":
+	app.run()
