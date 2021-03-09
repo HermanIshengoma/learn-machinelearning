3c0079da977e6a0d2f374108ef8246a21c51310b
Herman <hermani@cs.princeton.edu>
2021-03-08 22:49:09 -0500

    lesgooooo

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, make_response, redirect, url_for
 from flask import render_template, jsonify, session
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
 
@@ -9,6 +10,10 @@ def start_page():
 	html = render_template('index.html')
 	return html
 
+@app.route('/main', methods=['GET'])
+def main_page():
+	html = render_template('main.html')
+	return html
 
 if __name__ == "__main__":
 	app.run()
