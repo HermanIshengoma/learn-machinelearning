9b31f913522291310afbfc046e0ae533a2f03b4e
Herman <hermani@cs.princeton.edu>
2021-04-06 02:14:49 -0400

    indentation

--- app.py
+++ app.py
@@ -33,32 +33,32 @@ app.debug = True
 @app.errorhandler(Exception)
 def handle_auth_error(ex):
 	print('start page')
-    response = jsonify(message=str(ex))
-    response.status_code = (ex.code if isinstance(ex, HTTPException) else 500)
-    return response
+	response = jsonify(message=str(ex))
+	response.status_code = (ex.code if isinstance(ex, HTTPException) else 500)
+	return response
 
 oauth = OAuth(app)
 
 auth0 = oauth.register(
-    'auth0',
-    client_id=AUTH0_CLIENT_ID,
-    client_secret=AUTH0_CLIENT_SECRET,
-    api_base_url=AUTH0_BASE_URL,
-    access_token_url=AUTH0_BASE_URL + '/oauth/token',
-    authorize_url=AUTH0_BASE_URL + '/authorize',
-    client_kwargs={
-        'scope': 'openid profile email',
-    },
+	'auth0',
+	client_id=AUTH0_CLIENT_ID,
+	client_secret=AUTH0_CLIENT_SECRET,
+	api_base_url=AUTH0_BASE_URL,
+	access_token_url=AUTH0_BASE_URL + '/oauth/token',
+	authorize_url=AUTH0_BASE_URL + '/authorize',
+	client_kwargs={
+		'scope': 'openid profile email',
+	},
 )
 
 def requires_auth(f):
-    @wraps(f)
-    def decorated(*args, **kwargs):
-        if constants.PROFILE_KEY not in session:
-            return redirect('/index')
-        return f(*args, **kwargs)
+	@wraps(f)
+	def decorated(*args, **kwargs):
+		if constants.PROFILE_KEY not in session:
+			return redirect('/index')
+		return f(*args, **kwargs)
 
-    return decorated
+	return decorated
 
 # intro page
 @app.route('/', methods=['GET'])
@@ -70,42 +70,42 @@ def start_page():
 
 @app.route('/callback')
 def callback_handling():
-    auth0.authorize_access_token()
-    resp = auth0.get('userinfo')
-    userinfo = resp.json()
+	auth0.authorize_access_token()
+	resp = auth0.get('userinfo')
+	userinfo = resp.json()
 
-    session[constants.JWT_PAYLOAD] = userinfo
-    session[constants.PROFILE_KEY] = {
-        'user_id': userinfo['sub'],
-        'name': userinfo['name'],
-        'picture': userinfo['picture']
-    }
+	session[constants.JWT_PAYLOAD] = userinfo
+	session[constants.PROFILE_KEY] = {
+		'user_id': userinfo['sub'],
+		'name': userinfo['name'],
+		'picture': userinfo['picture']
+	}
 
     # dealing with the database
     database = Database(app)
-    database.connect()
-    userinfo=session[constants.PROFILE_KEY]
-    user_id=userinfo['user_id']
+	database.connect()
+	userinfo=session[constants.PROFILE_KEY]
+	user_id=userinfo['user_id']
 
-    success = database.user_exist(user_id)
-    if success is False:
-        new_username = "Stranger"
-        database.update_username(new_username, user_id)
-    database.disconnect()
+	success = database.user_exist(user_id)
+	if success is False:
+		new_username = "Stranger"
+		database.update_username(new_username, user_id)
+	database.disconnect()
 
-    return redirect('/main')
+	return redirect('/main')
 
 
 @app.route('/login')
 def login():
-    return auth0.authorize_redirect(redirect_uri=AUTH0_CALLBACK_URL, audience=AUTH0_AUDIENCE)
+	return auth0.authorize_redirect(redirect_uri=AUTH0_CALLBACK_URL, audience=AUTH0_AUDIENCE)
 
 
 @app.route('/logout')
 def logout():
-    session.clear()
-    params = {'returnTo': url_for('home', _external=True), 'client_id': AUTH0_CLIENT_ID}
-    return redirect(auth0.api_base_url + '/v2/logout?' + urlencode(params))
+	session.clear()
+	params = {'returnTo': url_for('home', _external=True), 'client_id': AUTH0_CLIENT_ID}
+	return redirect(auth0.api_base_url + '/v2/logout?' + urlencode(params))
 
 
 # linear algebra module
--- database.py
+++ database.py
@@ -17,69 +17,69 @@ CHAPTER_IDS = os.environ.get('CHAPTER_IDS').split(',')
 
 class Database:
 
-    def __init__(self, app=None):
-        self._conn = None
-        self._app = app
+	def __init__(self, app=None):
+		self._conn = None
+		self._app = app
 
     # connect to the PostgreSQL server
-    def connect(self):
-        try:
+	def connect(self):
+		try:
 
-            self._conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+			self._conn = psycopg2.connect(DATABASE_URL, sslmode='require')
 
-        except (Exception, psycopg2.DatabaseError) as error:
-            if self._app is not None:
-                self._app.logger.warning(str(error))
-            else:
-                print(str(error))
+		except (Exception, psycopg2.DatabaseError) as error:
+			if self._app is not None:
+				self._app.logger.warning(str(error))
+			else:
+				print(str(error))
 
 
     # close connection to the PostgreSQL server
-    def disconnect(self):
-        if self._conn is not None:
-            self._conn.close()
+	def disconnect(self):
+		if self._conn is not None:
+			self._conn.close()
 
 
     # add user if does not exist in database
     def user_exist(self, user_id):
-        try:
-            cur = self._conn.cursor()
-            # username = str(username)
-            cmd = "SELECT user_id FROM public.\"users\" WHERE user_id = CAST((%s) as text)"
-            cur.execute(cmd, (user_id, ))
-            if cur.fetchone() is None:
-                cmd2 = "INSERT INTO public.\"user\" (id, " + \
-                        "name)  " + \
-                        "VALUES (%s, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL)"
-                cur.execute(cmd2, (user_id, ))
-                self._conn.commit()
-                cur.close()
-                return False
+		try:
+			cur = self._conn.cursor()
+		    # username = str(username)
+			cmd = "SELECT user_id FROM public.\"users\" WHERE user_id = CAST((%s) as text)"
+			cur.execute(cmd, (user_id, ))
+			if cur.fetchone() is None:
+				cmd2 = "INSERT INTO public.\"user\" (id, " + \
+						"name)  " + \
+						"VALUES (%s, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL)"
+				cur.execute(cmd2, (user_id, ))
+				self._conn.commit()
+				cur.close()
+				return False
 
-            cur.close()
-            return True
-        except (Exception, psycopg2.DatabaseError) as error:
-            if self._app is not None:
-                self._app.logger.warning(str(error))
-            else:
-                print(str(error))
+		   	cur.close()
+		    return True
+		except (Exception, psycopg2.DatabaseError) as error:
+			if self._app is not None:
+	       		self._app.logger.warning(str(error))
+			else:
+				print(str(error))
 
 
     # update username in database
     def update_username(self, new_username, user_id):
-        try:
-            cur = self._conn.cursor()
-            cmd = "UPDATE public.\"users\" " + \
-                  "SET name = CAST((%s) as text) " + \
-                  "WHERE id = CAST((%s) as text)"
-            cur.execute(cmd, [new_username, user_id, ])
+		try:
+			cur = self._conn.cursor()
+			cmd = "UPDATE public.\"users\" " + \
+					"SET name = CAST((%s) as text) " + \
+					"WHERE id = CAST((%s) as text)"
+			cur.execute(cmd, [new_username, user_id, ])
 
-            self._conn.commit()
+			self._conn.commit()
 
             # close the communication with the PostgreSQL database
-            cur.close()
+			cur.close()
 
-        except (Exception, psycopg2.DatabaseError) as error:
-            self._app.logger.warning(str(error))
+		except (Exception, psycopg2.DatabaseError) as error:
+			self._app.logger.warning(str(error))
 
 
